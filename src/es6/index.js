36a9c0b7fd530e78c244bb947e33d58ac47b0b22
Anahi Quintero <anitaquintero99@gmail.com>
2021-02-06 16:15:22 -0600

    default params and concatenacion

---
+++ src/es6/index.js
@@ -0,0 +1,29 @@
+
+//version one
+function newfunction(name, age, country) {
+  var name = name || 'anahi';
+  var age = age || 22;
+  var country = country || 'CDMX';
+  console.log(name, age, country);
+}
+
+//es6
+
+function newFunction2(name='anahi', age=22, country='CDMX') {
+  console.log(name, age, country);
+}
+
+newFunction2();
+newFunction2('fernando', '24', 'MX');
+
+
+//phrase
+
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
