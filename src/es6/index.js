c5d0bc11d6a592bfb130a7caa8e847cc67233ecf
Anahi Quintero <anitaquintero99@gmail.com>
2021-02-10 17:31:24 -0600

    Add new exercise to multi lines

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,31 +1,38 @@
-
 //version one
 function newfunction(name, age, country) {
-  var name = name || 'anahi';
+  var name = name || "anahi";
   var age = age || 22;
-  var country = country || 'CDMX';
+  var country = country || "CDMX";
   console.log(name, age, country);
 }
 
 //es6
 
-function newFunction2(name='anahi', age=22, country='CDMX') {
+function newFunction2(name = "anahi", age = 22, country = "CDMX") {
   console.log(name, age, country);
 }
 
 newFunction2();
-newFunction2('fernando', '23', 'MX');
-
+newFunction2("fernando", "23", "MX");
 
 //phrase
 
-let hello = 'Hello';
-let world = 'World';
+let hello = "Hello";
+let world = "World";
 
-
-let epicPhrase = hello + ' ' + world;
+let epicPhrase = hello + " " + world;
 console.log(epicPhrase);
-
 
 let epicPhrase2 = `${hello} ${world}`;
 console.log(epicPhrase2);
+
+//js
+let lorem = "Una frase epica \n" + "otra frase epica para el programa.";
+
+// es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase
+`;
+
+console.log(lorem);
+console.log(lorem2);
