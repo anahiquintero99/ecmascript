07b7abbf908263d5d1c807011360fd58481410d5
Anahi Quintero <anitaquintero99@gmail.com>
2021-02-10 20:19:56 -0600

    Add new exercise about ems7

---
+++ src/es6/es7/index.js
@@ -0,0 +1,16 @@
+//include to search a value
+
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log("Si se encuentra el valor 7");
+} else {
+  console.log("No se encuentra.");
+}
+
+//exponent
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
