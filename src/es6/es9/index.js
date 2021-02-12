af6b45a048a6582324e5cb10162e1b376f6e375a
Anahi Quintero <anitaquintero99@gmail.com>
2021-02-11 22:12:11 -0600

    Add new exercise about ems9 to promise

--- src/es6/es9/index.js
+++ src/es6/es9/index.js
@@ -20,3 +20,16 @@ const obj1 = {
 console.log(obj1);
 
 //Juntar objetos
+
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Hello World"), 3000)
+      : reject(new Error("Test Error"));
+  });
+};
+
+helloWorld()
+  .then((resonse) => console.log(resonse))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Finalizo"));
