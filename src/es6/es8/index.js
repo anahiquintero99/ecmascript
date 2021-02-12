6a7ac527df106c41dcf28d036635ce6507afa1e9
Anahi Quintero <anitaquintero99@gmail.com>
2021-02-11 21:03:25 -0600

    Add new exercise about ems8 to values

--- src/es6/es8/index.js
+++ src/es6/es8/index.js
@@ -9,3 +9,14 @@ console.log(entries);
 console.log(entries.length);
 
 //Regresa una matriz
+
+const data = {
+  fronted: "Anahi",
+  backend: "Angel",
+  desig: "Arely",
+};
+
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
+//solo se imprime los valores.
