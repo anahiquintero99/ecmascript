a70620779a72f8ba993f1078ef62f1c6ecfa9ce6
Anahi Quintero <anitaquintero99@gmail.com>
2021-02-11 20:50:00 -0600

    Add new exercise about ems8

---
+++ src/es6/es8/index.js
@@ -0,0 +1,11 @@
+const data = {
+  fronted: "Anahi",
+  backend: "Angel",
+  desig: "Arely",
+};
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+//Regresa una matriz
