0079c65c39b7f3ce5a37a6d7c87ac8151720989d
Anahi Quintero <anitaquintero99@gmail.com>
2021-02-11 21:41:08 -0600

    Add new exercise about ems8 to async

---
+++ src/es6/es8/async.js
@@ -0,0 +1,14 @@
+const helloWorld = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Hello Wolrd", 3000))
+      : reject(new Error("Test Error"));
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+};
+
+helloAsync();
